1539de75a29519c3123e23e2b1da971c68b1d1f6
DStudentDeveloper <int22011532@central.edu.gh>
2024-11-17 17:23:04 +0000

    🚀 initial commit

---
+++ android/app/src/main/kotlin/co/akundadababalei/course_rep_management_panel/MainActivity.kt
@@ -0,0 +1,5 @@
+package co.akundadababalei.course_rep_management_panel
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:course_rep_management_panel/app/app.dart';
+import 'package:course_rep_management_panel/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
