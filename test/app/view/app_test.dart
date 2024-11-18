d3f4afb3ac3d17b51f55364e55e427cccac8af13
DStudentDeveloper <int22011532@central.edu.gh>
2024-11-18 10:30:06 +0000

    🧪 Test: Refactor app_test.dart to render `LoginScreen`

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,26 @@
-import 'package:course_rep_management_panel/app/app.dart';
-import 'package:course_rep_management_panel/counter/counter.dart';
+import 'package:course_rep_management_panel/src/auth/presentation/views/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../helpers/pump_app.dart';
+
+class MockFirebaseApp extends Mock implements FirebaseApp {}
+
+class MockFirebaseAuth extends Mock implements FirebaseAuth {
+  @override
+  FirebaseApp get app => MockFirebaseApp();
+}
 
 void main() {
+  setUpAll(() => setFirebaseUiIsTestMode(true));
+
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+    testWidgets('renders LoginScreen', (tester) async {
+      await tester.pumpApp(LoginScreen(auth: MockFirebaseAuth()));
+      expect(find.byType(LoginScreen), findsOneWidget);
     });
   });
 }
